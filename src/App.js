570f593812904b0ac921140e921b0a748ebf7c52
roma_marshall <roma_marshall@outlook.com>
2022-05-31 14:34:43 +0200

    logic app added

--- src/App.js
+++ src/App.js
@@ -9,6 +9,33 @@ class App extends Component {
         }
     }
 
+    updateInput(key, value) {
+        // update react state
+        this.setState({
+            [key]: value
+        });
+    }
+    addItem() {
+        // create item with unique id
+        const newItem = {
+            id: 1 + Math.random(),
+            value: this.state.newItem.slice()
+        };
+
+        // copy of current list of items
+        const list = [...this.state.list];
+
+        // add new item to list
+        list.push(newItem);
+
+        // update state with new list and reset newItem input
+        this.setState({
+            list,
+            newItem: ''
+        });
+
+    }
+
     render() {
         return (
             <div className={App}>
