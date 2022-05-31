e902b0a5a1a29e9681ae069c0f1c4ebdb2f130b5
roma_marshall <roma_marshall@outlook.com>
2022-05-31 15:15:12 +0200

    tailwind styling

--- src/App.js
+++ src/App.js
@@ -49,34 +49,42 @@ class App extends Component {
     render() {
         return (
             <div className={App}>
-                <div>
-                    Add an Item
-                    <br/>
-                    <input type="text"
-                           placeholder="type here..."
-                           value={this.state.newItem}
-                           onChange={e => this.updateInput("newItem", e.target.value)}
-                    />
-                    <button
-                        onClick={() => this.addItem()}
-                    >
-                        Add
-                    </button>
-                    <br/>
-                    <ul>
-                        {this.state.list.map(item => {
-                            return (
-                                <li key={item.id}>
-                                    {item.value}
-                                    <button
-                                        onClick={() => this.deleteItem(item.id)}
-                                    >
-                                        X
-                                    </button>
-                                </li>
-                            )
-                        })}
-                    </ul>
+                <div className="flex items-center justify-between w-1/2 border-gray-900/20">
+                    <div className="flex flex-col w-1/2 m-10">
+                    <span className="text-xl mb-5 p-2">Add an Item</span>
+                        <div className="">
+                            <input
+                                className="border p-2 rounded-xl"
+                                type="text"
+                                placeholder="type here..."
+                                value={this.state.newItem}
+                                onChange={e => this.updateInput("newItem", e.target.value)}
+                            />
+                            <button
+                                className="font-bold ml-3 py-2 px-4 rounded bg-blue-500 text-white hover:bg-blue-700"
+                                onClick={() => this.addItem()}
+                            >
+                                Add
+                            </button>
+                        </div>
+                        <div className="mt-5">
+                            {this.state.list.map(item => {
+                                return (
+                                    <div
+                                        className="flex justify-between border-b color-gray-900/20 mb-2 pb-2"
+                                        key={item.id}>
+                                        <span className="mr-10 pt-1"> {item.value}</span>
+                                        <button
+                                            className="font-bold py-1 px-3 rounded bg-red-500 text-white hover:bg-red-700"
+                                            onClick={() => this.deleteItem(item.id)}
+                                        >
+                                            X
+                                        </button>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    </div>
                 </div>
             </div>
         );
