cf584080b5ee80a5c8045d7403416eed665c5136
roma_marshall <roma_marshall@outlook.com>
2022-05-31 14:30:54 +0200

    form react added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React, {Component} from "react";
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            newItem: '',
+            list: []
+        }
+    }
+
     render() {
         return (
             <div className={App}>
-                ReactApp
+                <div>
+                    Add an Item
+                    <br/>
+                    <input type="text"
+                           placeholder="type here..."
+                           value={this.state.newItem}
+                           onChange={e => this.updateInput("newItem", e.target.value)}
+                    />
+                    <button
+                        onClick={() => this.addItem()}
+                    >
+                        Add
+                    </button>
+                </div>
             </div>
         );
     }
